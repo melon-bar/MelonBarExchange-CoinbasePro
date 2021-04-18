86898af7531f6a36c304178cfdcd4aed45811f6c
coryjk <36049396+coryjk@users.noreply.github.com>
2021-04-18 02:34:55 -0400

    Even more javadoc

--- src/main/java/com/coinbase/model/account/AccountsRequest.java
+++ src/main/java/com/coinbase/model/account/AccountsRequest.java
@@ -5,10 +5,16 @@ import com.coinbase.model.request.BaseRequest;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * Requests for Coinbase Pro accounts API.
+ */
 @Getter
 @Builder
 public class AccountsRequest extends BaseRequest {
 
+    /**
+     * Account ID.
+     */
     @RequestField(index = 0)
     private final String accountId;
 }
