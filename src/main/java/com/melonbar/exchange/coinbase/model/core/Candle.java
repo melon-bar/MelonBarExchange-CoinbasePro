c3b35bb9649a582d0248fb4417582a2e6961aad7
coryjk <36049396+coryjk@users.noreply.github.com>
2021-05-31 12:51:36 -0700

    Move Candle definition and include bearish/bullish eval

--- src/main/java/com/melonbar/exchange/coinbase/model/core/Candle.java
+++ src/main/java/com/melonbar/exchange/coinbase/model/core/Candle.java
@@ -1,4 +1,4 @@
-package com.melonbar.exchange.coinbase.model.products;
+package com.melonbar.exchange.coinbase.model.core;
 
 import org.joda.time.DateTime;
 
@@ -12,9 +12,13 @@ public record Candle(DateTime startTime, BigDecimal low, BigDecimal high,
         if (fields.length != 6) {
             throw new IllegalArgumentException("Invalid number of detected fields for candle: " + bucket);
         }
-        return new Candle(new DateTime(Long.parseLong(fields[0])),
+        return new Candle(new DateTime(Long.parseLong(fields[0])*1000),
                 new BigDecimal(fields[1]), new BigDecimal(fields[2]),
                 new BigDecimal(fields[3]), new BigDecimal(fields[4]),
                 new BigDecimal(fields[5]));
+    }
+
+    public Sentiment getType() {
+        return close.compareTo(open) > 0 ? Sentiment.BULLISH : Sentiment.BEARISH;
     }
 }
---
+++ src/main/java/com/melonbar/exchange/coinbase/model/core/Sentiment.java
@@ -0,0 +1,6 @@
+package com.melonbar.exchange.coinbase.model.core;
+
+public enum Sentiment {
+    BEARISH,
+    BULLISH;
+}
